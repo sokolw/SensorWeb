3805a84687eba90f7b0ab4ec607bb753b25529eb
sokolw <sokolw21@gmail.com>
2022-05-27 14:31:52 +0300

    feat: add autoget identity, replace comments

--- Program.cs
+++ Program.cs
@@ -4,20 +4,30 @@ using SensorWeb.Sensor.DataAccess.Repository;
 using SensorWeb.Sensor.DataAccess.Repository.IRepository;
 using SensorWeb.Sensor.DataAccess.RequestAPI;
 using SensorWeb.Sensor.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+// identity autogen
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+// identity autogen
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-// удобный сервис дл€ динамической компл€ции разор страниц
+// useful service for dynamically compile RazorPages
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-// тож в DI
+// use DI
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-// делаем DI зависимость
-builder.Services.AddScoped<ISensorModelRepository, SensorModelRepository>(); // подключаем парттерн Repository 
-// сервис дл€ получени€ данных по апи
+// use DI
+builder.Services.AddScoped<ISensorModelRepository, SensorModelRepository>(); // use in service pattern-Repository 
+// add service to access API
 builder.Services.AddScoped<DataFromAPI>();
 
 var app = builder.Build();
@@ -34,6 +44,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
