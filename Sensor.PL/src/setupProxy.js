f496dd8ee860ad4f334db32ab3965068414a6966
sokolw <sokolw21@gmail.com>
2022-05-30 22:51:06 +0300

    fix: path in setupProxy.js

--- Sensor.PL/src/setupProxy.js
+++ Sensor.PL/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:59184';
 
 const context =  [
-  "/weatherforecast",
+  "/sensorTEST123456789",
 ];
 
 module.exports = function(app) {
